933d872e815c8133a77cf7ce36f506c046093444
sahar <saharnasimi96@gmail.com>
2022-06-18 23:39:56 +0430

    rogoz file added

--- launch/drone_launch.py
+++ launch/drone_launch.py
@@ -1,7 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
+    pkg_name = 'drone_sim'
+    pkg_dir = os.popen('/bin/bash -c "source /usr/share/colcon_cd/function/colcon_cd.sh && \
+        colcon_cd %s && pwd"' % pkg_name).read().strip()
     return LaunchDescription([
         Node(
             package='drone_sim',
@@ -20,6 +24,6 @@ def generate_launch_description():
             namespace='',
             executable='rviz2',
             name='rviz2',
-            # arguments=['-d', [os.path.join(pkg_dir, 'config', 'config_file.rviz')]]
+            arguments=['-d', [os.path.join(pkg_dir, 'config', 'config_file.rviz')]]
         )
     ])
